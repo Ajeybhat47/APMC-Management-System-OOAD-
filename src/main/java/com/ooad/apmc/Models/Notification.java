ac0d002e130094815f4b8f98334f498f23846e22
Ajeybhat47 <ajeybhatm@gmail.com>
2024-04-13 21:54:55 +0530

    updated Notification class

--- src/main/java/com/ooad/apmc/Models/Notification.java
+++ src/main/java/com/ooad/apmc/Models/Notification.java
@@ -8,7 +8,10 @@ public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long notificationId;
+    
+    @Column(name = "message")
     private String message;
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
